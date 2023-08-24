a1e1e899d606a4afe8929d1cd4101caef4072cf7
Rifzkhy <sampit215@gmail.com>
2023-08-25 00:10:16 +0700

    fix

--- script/global.js
+++ script/global.js
@@ -2,74 +2,89 @@ document.addEventListener("contextmenu", (event) => {
   event.preventDefault();
 });
 
-const texts = ["I'm Rifzkhy", "I'm Programmer", "I'm Desainer"];
+const texts = ["I'm Rifzkhy", "I'm Programmer", "I'm Designer"];
 
-const typingElement = document.querySelector(".typing");
-let textIndex = 0;
+const typingElements = document.querySelectorAll(".typing");
 
-function type() {
+function type(element, text) {
   let i = 0;
-  const text = texts[textIndex];
+
   const typingInterval = setInterval(() => {
     if (i < text.length) {
-      typingElement.textContent += text.charAt(i);
+      element.textContent += text.charAt(i);
       i++;
     } else {
       clearInterval(typingInterval);
-      setTimeout(erase, 1000); // Wait for a moment before erasing
+      setTimeout(() => erase(element), 1000);
     }
-  }, 200); // Adjust the typing speed by changing the interval (e.g., 50 for faster typing).
+  }, 200);
 }
 
-function erase() {
+function erase(element) {
+  const text = element.textContent;
+
   const typingInterval = setInterval(() => {
-    if (typingElement.textContent.length > 0) {
-      typingElement.textContent = typingElement.textContent.slice(0, -1);
+    if (element.textContent.length > 0) {
+      element.textContent = element.textContent.slice(0, -1);
     } else {
       clearInterval(typingInterval);
-      textIndex = (textIndex + 1) % texts.length; // Move to the next text
-      setTimeout(type, 500); // Wait for a moment before typing the next text
+      setTimeout(() => {
+        textIndex = (textIndex + 1) % texts.length;
+        type(element, texts[textIndex]);
+      }, 500);
     }
-  }, 50); // Adjust the erasing speed if needed
+  }, 50);
 }
 
-window.addEventListener("load", () => {
-  $(window).on("load", function () {
-    $("html, body").animate({ scrollTop: 0 });
-    $(".loader").fadeOut(1000);
-    $(".content").fadeIn(1000);
-    setTimeout(type, 1000);
+function checkInView() {
+  typingElements.forEach((element) => {
+    const rect = element.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      // Element is in view, start typing effect
+      if (element.textContent === "") {
+        textIndex = (textIndex + 1) % texts.length;
+        type(element, texts[textIndex]);
+      }
+    } else {
+      // Element is out of view, reset effect
+      element.textContent = "";
+    }
   });
-});
+}
 
+// Check if elements are in view when the page loads
+checkInView();
+
+// Check if elements are in view when the user scrolls
+window.addEventListener("scroll", checkInView);
+
+//! Scroll effect
 const scrollElements = document.querySelectorAll(".js-scroll");
 
-const elementInView = (el, dividend = 1) => {
+function elementInView(el, dividend = 1) {
   const elementTop = el.getBoundingClientRect().top;
-
   return (
     elementTop <=
     (window.innerHeight || document.documentElement.clientHeight) / dividend
   );
-};
+}
 
-const elementOutofView = (el) => {
+function elementOutofView(el) {
   const elementTop = el.getBoundingClientRect().top;
-
   return (
     elementTop > (window.innerHeight || document.documentElement.clientHeight)
   );
-};
+}
 
-const displayScrollElement = (element) => {
+function displayScrollElement(element) {
   element.classList.add("scrolled");
-};
+}
 
-const hideScrollElement = (element) => {
+function hideScrollElement(element) {
   element.classList.remove("scrolled");
-};
+}
 
-const handleScrollAnimation = () => {
+function handleScrollAnimation() {
   scrollElements.forEach((el) => {
     if (elementInView(el, 1.25)) {
       displayScrollElement(el);
@@ -77,8 +92,44 @@ const handleScrollAnimation = () => {
       hideScrollElement(el);
     }
   });
-};
+}
 
-window.addEventListener("scroll", () => {
-  handleScrollAnimation();
-});
+// Check if elements are in view when the page loads
+handleScrollAnimation();
+
+// Check if elements are in view when the user scrolls
+window.addEventListener("scroll", handleScrollAnimation);
+
+//! Writing effect
+const elements = document.querySelectorAll(".write");
+
+function type(element) {
+  const text = element.textContent;
+  element.textContent = "";
+
+  let i = 0;
+
+  function typeCharacter() {
+    if (i < text.length) {
+      element.textContent += text.charAt(i);
+      i++;
+      setTimeout(typeCharacter, 200);
+    }
+  }
+
+  function checkInView() {
+    const rect = element.getBoundingClientRect();
+    if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+      // Element is in view, start typing effect
+      typeCharacter();
+    }
+  }
+
+  // Check if element is in view when the page loads
+  checkInView();
+
+  // Check if element is in view when the user scrolls
+  window.addEventListener("scroll", checkInView);
+}
+
+elements.forEach(type);
