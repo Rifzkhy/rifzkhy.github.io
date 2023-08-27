2f95d7be7bc828f32c549444580959b63f12cb12
Rifzkhy <sampit215@gmail.com>
2023-08-27 21:27:05 +0700

    fix

--- script/global.js
+++ script/global.js
@@ -1,3 +1,19 @@
+document.onreadystatechange = function () {
+  if (document.readyState !== "complete") {
+    document.querySelector(".content").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    setTimeout(function () {
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector(".content").style.visibility = "visible";
+    }, 1000);
+  }
+};
+
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
+
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
@@ -25,3 +41,44 @@ function topFunction() {
 }
 
 mybutton.addEventListener("click", topFunction);
+
+let experience = document.querySelectorAll('.experience');
+
+    window.onscroll = () => {
+      experience.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 250;
+        let height = sec.offsetHeight;
+
+        if (top >= offset && top < offset + height) {
+          sec.classList.add('experience-animate');
+        } 
+      })
+}
+    
+// TODO: Skill bar animation
+function moveAlldesign() {
+  var skillBars = document.querySelectorAll(".myBardesign");
+
+  skillBars.forEach(function (myBardesign) {
+    var width = 10;
+    var numericValue = parseFloat(myBardesign.getAttribute("data-percent"));
+    var i = 0;
+
+    if (i == 0) {
+      i = 1;
+      var id = setInterval(frame, 10);
+
+      function frame() {
+        if (width >= numericValue) {
+          clearInterval(id);
+          i = 0;
+        } else {
+          width++;
+          myBardesign.style.width = width + "%";
+          myBardesign.innerHTML = width + "%";
+        }
+      }
+    }
+  });
+}
